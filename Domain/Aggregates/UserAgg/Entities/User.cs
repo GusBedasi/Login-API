01c030947242b6a0c7f400582773066fd29941cb
GusBedasi <ggustavobedasi@gmail.com>
2021-07-09 01:52:30 -0300

    initial commit

---
+++ Domain/Aggregates/UserAgg/Entities/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Domain.Aggregates.UserAgg.Entities
+{
+    public class User
+    {
+        public User(bool active)
+        {
+            CreatedAt = DateTime.UtcNow;
+            UpdatedAt = DateTime.UtcNow;
+            Active = active;
+        }
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        private DateTime CreatedAt { get; }
+        private DateTime? UpdatedAt { get; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string CryptedPassword { get; set; }
+        private bool Active { get; set; }
+
+        public void Enable(bool enable)
+        {
+            Active = enable;
+        }
+        
+        public bool IsActive()
+        {
+            return Active;
+        }
+    }
+}
---
+++ Service.API/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Service.API.Controllers
+{
+    [Route("login")]
+    [ApiController]
+    public class LoginController : Controller
+    {
+        private ILogger<LoginController> _logger;
+        private ILoginService _loginService;
+        
+        public LoginController(ILogger<LoginController> logger, ILoginService loginService)
+        {
+            _logger = logger;
+            _loginService = loginService;
+        }
+        
+        
+    }
+}
