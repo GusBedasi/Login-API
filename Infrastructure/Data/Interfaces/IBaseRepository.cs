07146ffd2c805aaa19190790503721cf88587acd
GusBedasi <ggustavobedasi@gmail.com>
2021-07-10 00:14:06 -0300

    adding repository pattern

---
+++ Infrastructure/Data/Interfaces/IBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Domain.Contracts;
+
+namespace Infrastructure.Data.Interfaces
+{
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        void Add(TEntity obj);
+        void Update(TEntity obj);
+        void Delete(TEntity obj);
+        TEntity FindOne(Expression<Func<TEntity, bool>> filter);
+        List<TEntity> FindAll(Expression<Func<TEntity, bool>> filter);
+    }
+}
---
+++ Infrastructure/Data/Repositories/BaseRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Infrastructure.Data.Context;
+using Infrastructure.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data.Repositories
+{
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
+    {
+        private readonly LoginDbContext _context;
+        public BaseRepository(LoginDbContext context)
+        {
+            _context = context;
+        }
+        
+        public void Add(TEntity obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            _context.Add(obj);
+        }
+
+        public void Update(TEntity obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            _context.Entry(obj).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Delete(TEntity obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentException();
+            }
+
+            _context.Remove(obj);
+            _context.SaveChanges();
+        }
+
+        public TEntity FindOne(Expression<Func<TEntity, bool>> filter)
+        {
+            return _context.Set<TEntity>().AsNoTracking().FirstOrDefault(filter);
+        }
+
+        public List<TEntity> FindAll(Expression<Func<TEntity, bool>> filter)
+        {
+            return _context.Set<TEntity>().AsNoTracking().ToList();
+        }
+    }
+}
